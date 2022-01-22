5900cf371242737b7807c6205d3c7a5e90104670
fem97 <fem.nwu@gmail.com>
2022-01-22 12:17:56 +0600

    updated

--- L-21-expressRouter/publicRoute.js
+++ L-21-expressRouter/publicRoute.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const publicRouter = express.Router();
 
+const mw1 = (req, res, next) => {
+    console.log("middleware...");
+    next();
+}
+
+publicRouter.all('*', mw1);
+
 publicRouter.get('/', (req, res) => {
     res.send("Public Dashboard!");
 })
