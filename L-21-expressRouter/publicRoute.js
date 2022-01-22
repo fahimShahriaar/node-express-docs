562cda263a091f23f1608e1f27173a530e700dec
fem97 <fem.nwu@gmail.com>
2022-01-22 14:19:19 +0600

    updated

--- L-21-expressRouter/publicRoute.js
+++ L-21-expressRouter/publicRoute.js
@@ -8,8 +8,38 @@ const mw1 = (req, res, next) => {
 
 publicRouter.all('*', mw1);
 
-publicRouter.get('/', (req, res) => {
+publicRouter
+    .route('/post')
+    .all((req, res, next) => {
+        console.log("logging something");
+        next();
+    })
+    .get((req, res) => {
+        res.send("All posts will be send");
+    })
+    .post((req, res) => {
+        res.send("Post will posted");
+    })
+    .delete((req, res) => {
+        res.send("Post will be deleted");
+    })
+    .put((req, res) => {
+        res.send("Post will updated");
+    })
+
+publicRouter.param('user', (req, res, next, id) => {
+    console.log(id);
+    req.user = id === "1" ? "Admin" : "Anonymous";
+
+    next();
+})
+
+publicRouter.get('/:user', (req, res) => {
     res.send("Public Dashboard!");
+})
+
+publicRouter.get('/:user/about', (req, res) => {
+    res.send(`Public Information of ${req.user}`);
 })
 
 module.exports = publicRouter;
