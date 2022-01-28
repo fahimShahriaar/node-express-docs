6205a676bce47b439256945b63966ea4a7bc3b7f
fem97 <fem.nwu@gmail.com>
2022-01-28 20:29:38 +0600

    updated

--- L-26-mongooseCrud/index.js
+++ L-26-mongooseCrud/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const todoHandler = require('./routeHandler/todoHandler')
+const todoHandler = require('./routeHandler/todoHandler');
+const userHandler = require('./routeHandler/userHandler');
 
 // express app initialization
 const app = express();
@@ -14,6 +15,7 @@ mongoose
 
 // Application routes
 app.use('/todo', todoHandler);
+app.use('/user', userHandler);
 
 
 // Default Error Handler
@@ -27,4 +29,4 @@ function errorHandler(err, req, res, next) {
 app.use(errorHandler);
 
 // listening server
-app.listen(5000, () => console.log("Server running on port: 5000"));
+app.listen(5001, () => console.log("Server running on port: 5000"));
