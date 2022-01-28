9c2c67048b3361f5939a80074c27b618cd796db3
fem97 <fem.nwu@gmail.com>
2022-01-28 20:44:51 +0600

    updated

--- L-26-mongooseCrud/routeHandler/userHandler.js
+++ L-26-mongooseCrud/routeHandler/userHandler.js
@@ -1,27 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
 const userSchema = require('../schemas/userSchema');
-
 const User = mongoose.model('User', userSchema);
 
-// SIGN UP
-router.post('/signup', async (req, res) => {
-    try {
-        const password = await bcrypt.hash(req.body.password, 10);
-        const newUser = new User({
-            name: req.body.name,
-            username: req.body.username,
-            password: password
-        })
-        const result = await newUser.save();
-        res.send(result);
-    } catch (error) {
-        res.send(error);
-    }
+router.post('/signup', (req, res) => {
+    res.send("Hello")
 })
 
 module.exports = router;
